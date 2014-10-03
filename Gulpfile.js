dbed2d9bd21a5319de36af55fc67258514c288e0
Kevin Wang <wangkevin448@gmail.com>
2014-10-02 22:15:26 -0700

    secondary commit

---
+++ Gulpfile.js
@@ -0,0 +1,13 @@
+//GULP 
+var gulp = require('gulp');
+
+//GULP PLUGINS 
+var jshint = require('gulp-jshint');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var changed = require('gulp-changed');
+var clean = require('gulp-clean');
+var util = require('gulp-util');
+
